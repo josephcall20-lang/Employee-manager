c9941401a0c45a7fbdaf240466cae180f440cfea
josephcall20-lang <228448408+josephcall20-lang@users.noreply.github.com>
2025-09-07 17:51:42 -0700

    added main

---
+++ frontend/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+// If you have a main CSS file, you would import it here, e.g.:
+// import './index.css' 
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
